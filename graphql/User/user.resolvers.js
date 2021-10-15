8ba228e0d4f0c5e45cf585831ba21466a207564a
Alex Nikanov <alekseynikanov.21@gmail.com>
2021-10-15 18:21:11 -0400

    docs: :bulb: Updates some comments/documentation

--- graphql/User/user.resolvers.js
+++ graphql/User/user.resolvers.js
@@ -1,11 +1,16 @@
-const { getUserById, getUserByEmail, getUsers } = require('../../api/user.api');
-const { User } = require('./user.types');
+const { getUserById, getUserByEmail, getUsers } = require('../../database/user.api');
 
+/**
+ * Contains all the main query and mutation resolvers that operate with User. 
+ * 
+ * Note that one shouldn't actually interact with the database here. Instead, make use of the api functions.
+ */
 const userResolvers = {
     Query: {
         users: () => {
             return getUsers().then();
         },
+
         user: (parent, args, context, info) => {
             return getUserById(args.id).then();
         },
--- graphql/schema.js
+++ graphql/schema.js
@@ -5,6 +5,8 @@ const { User } = require('./User/user.types');
 const { userResolvers } = require('./User/user.resolvers');
 
 // Note that we must have at least one field (empty in this case)
+// In this set up, we extend the root Query type within individual classes, 
+// which allows for more flexibility and compatibility. Read the docs for more info on this.
 const Query = `
     type Query {
         _empty: String
