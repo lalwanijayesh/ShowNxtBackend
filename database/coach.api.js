513d17b8cd384e0a94849362c1bc2b6883de5577
Nick Salesky <nicksalesky@gmail.com>
2021-10-16 17:38:02 -0400

    Attempted to implement createCoach

--- database/coach.api.js
+++ database/coach.api.js
@@ -21,20 +21,23 @@ const getCoachById = async (id) => {
   return res.rows[0];
 };
 
-// Todo: figure out how to do `createCoach`
+// is this function for createCoach correct?
 
 /**
  * Creates a new coach in the database.
  *
- * @param {String} email the email of the user we are creating
- * @param {UserType} type the type of the user from the UserType enum
- * @returns the user if the creation is successful, otherwise throws an error
+ * @param {Number} id the id of the user for the coach we are creating
+ * @param {School} school the school of the coach we are creating
+ * @param {Sport} sport the sport overseen by the coach we are creating
+ * @param {String} firstName the first name of the coach we are creating
+ * @param {String} lastName the last name of the coach we are creating
+ * @returns the coach if the creation is successful, otherwise throws an error
  */
-const createCoach = async (email, type) => {
-  await pool.query("INSERT INTO coaches (email, type) VALUES ($1, $2)", [
-    email,
-    type,
-  ]);
+const createCoach = async (id, school, sport, firstName, lastName) => {
+  await pool.query(
+    "INSERT INTO coaches (id, schoolID, sportID, firstName, lastName) VALUES ($1, $2, $3, $4, $5)",
+    [id, school.id, sport.id, firstName, lastName]
+  );
 
-  return await getUserByEmail(email);
+  return await getCoachById(id);
 };
